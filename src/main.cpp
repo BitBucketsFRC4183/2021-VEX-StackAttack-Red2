7c3bde1b1d6f031963236cf3fd08a29e9d375a59
Unknown <nitishbhamidipati@gmail.com>
2021-03-14 16:22:06 -0700

    Directions
    Allows robot to move in all 4 directions if left joystick on controller is used for forward, backward and right joystick on the controller is used for left and right

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,40 @@
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
 // Controller1          controller                    
-// Drivetrain           drivetrain    1, 2, 3, 4      
-// Vision5              vision        5               
+// Drivetrain           drivetrain    1, 2            
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
 
+void driveRobot() {
+  if (Controller1.Axis3.position() == 0){
+    Drivetrain.stop();
+  }
+
+  else if (Controller1.Axis3.position() > 0){
+    Drivetrain.drive(forward);
+  }
+  else {
+    Drivetrain.drive(reverse);
+  }
+}
+
+void turnRobot(){
+  if (Controller1.Axis1.position() > 0){
+    Drivetrain.turn(right);
+  }
+  else {
+    Drivetrain.turn(left);
+  } 
+}
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  Drivetrain.setDriveVelocity(50, percent);
-  Drivetrain.drive(forward);
   
+  Drivetrain.setDriveVelocity(50,percent);
+
+  Controller1.Axis3.changed(driveRobot);
 }
