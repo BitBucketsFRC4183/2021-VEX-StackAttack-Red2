030b85fa1c4290d150497cd75e52ee218c255196
Unknown <nitishbhamidipati@gmail.com>
2021-03-14 17:55:01 -0700

    drive motor config changes
    changed motor ports for drivebase

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -9,8 +9,8 @@ brain  Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
-motor LeftDriveSmart = motor(PORT1, ratio18_1, false);
-motor RightDriveSmart = motor(PORT2, ratio18_1, true);
+motor LeftDriveSmart = motor(PORT10, ratio18_1, false);
+motor RightDriveSmart = motor(PORT1, ratio18_1, true);
 drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
 motor Motor3 = motor(PORT3, ratio18_1, false);
 motor Motor8 = motor(PORT8, ratio18_1, false);
